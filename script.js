de928dc627f6f02df09d12a5975f95c2ac45db31
vigneshpyc <145823761+vigneshpyc@users.noreply.github.com>
2024-06-07 20:30:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function navbar(){
+    document.getElementById("nav-bar2").style.display = "block";
+    document.getElementById("navimg").style.display = "none";
+    document.getElementById("cross").style.display = "block";
+
+}
+function cancel(){
+    document.getElementById("nav-bar2").style.display = "none";
+    document.getElementById("navimg").style.display = "block";
+    document.getElementById("cross").style.display = "none" ;
+}
+function open(){
+    window.open("https://www.linkedin.com/in/vignesh-g-vicky-ba579a274/")
+}
